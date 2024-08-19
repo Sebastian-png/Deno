ea48116386b9890f7b78785f5fe9e2fbcca8114d
Sebastian Ballen Loaiza <sballen@uniempresarial.edu.co>
2024-08-19 12:38:25 -0500

    cambio

--- crud/app.ts
+++ crud/app.ts
@@ -1,10 +1,8 @@
 import { Application } from "./deps.ts";
 import router from "./routes.ts";
-
 const app = new Application();
-
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-console.log("Server running on http://localhost:8000");
-await app.listen({ port: 8000 });
+const port = Number(Deno.env.get("PORT")) || 8000;
+console.log(`Server is running on port ${port}`);
+await app.listen({ port });
--- crud/deps.ts
+++ crud/deps.ts
@@ -1,2 +1,2 @@
-export { Application, Router } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v8.0.0/mod.ts";
 export { Client } from "https://deno.land/x/mysql@v2.12.1/mod.ts";
